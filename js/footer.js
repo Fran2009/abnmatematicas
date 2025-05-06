c7f3bca6a15ae8609801188afb527af83dd3955b
fran2009 <fran2009@gmail.com>
2025-05-06 09:54:32 +0200

    Se cambia enlace del footer directamente al proyecto de guthub

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const footer = document.createElement("footer");
     footer.innerHTML = `
       Web creada por Fran Tirado · 
-      <a href="https://github.com/Fran2009" target="_blank" aria-label="GitHub">
+      <a href="https://github.com/Fran2009/abnmatematicas/" target="_blank" aria-label="GitHub">
         <i class="fab fa-github"></i> GitHub
       </a>
     `;
